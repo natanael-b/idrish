f6b4bb31404c5f40377095b60cccf4714b74685b
Natanael Barbosa Santos <104917678+natanael-b@users.noreply.github.com>
2023-10-02 13:33:22 -0300

    Install Lua in the test environment

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+sudo apt install lua5.4 > /dev/null 2>&1
+
 input='crie o arquivo teste.txt, coloque nele a frase Olá Mundo!, abra no kate e mova ele para /tmp'
 
 hash="6f3cc0c9c147c2aad6f207dedae5a3b498256704c4f0d1fdc77316428b69075f"
